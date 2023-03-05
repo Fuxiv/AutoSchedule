d3164b24ed8a127c111a1782637902f0b5f7fde5
Jakub Szymański <simple.jacob@icloud.com>
2023-03-05 23:54:47 +0100

    done: redone the database system (not  yet) to add: generate all files(in progress) necessary to submit the form. maybe automize form submission? IDEAS: add 'grafik' - automatically generate schedule report from the driver schedule change database system: one table for all trips, trip index as time, in case of a not full route, jump to the next one until it's not null.[DONE]

--- src/main/java/org/example/Connect.java
+++ src/main/java/org/example/Connect.java
@@ -8,55 +8,84 @@ public class Connect{
     public static short linia;
     public static short kurs;
 
-    public Connection connect(short linia){
-        Connect.linia = linia;
-        String url = "jdbc:sqlite:C:\\Users\\Jackobe\\IdeaProjects\\autoschedule\\src\\main\\java\\org\\example\\" + linia;
+    public Connection connect(){
+        String url = "jdbc:sqlite:src/main/java/org/example/baza";
         Connection conn = null;
         try{
             conn = DriverManager.getConnection(url);
         } catch(SQLException e){
+            e.printStackTrace();
             System.out.println(e.getMessage());
         }
         return conn;
     }
 
     ArrayList<String> output = new ArrayList<>();
-    short id;
+    int id;
 
+//    public void get(){
+//        try {
+//            Connection conn = this.connect();
+//            Statement stmt = conn.createStatement();
+//            for (int i = 2; i<=6; i++){
+//                String czasQuery = "select 10"+i+", przystanek from 813 where czas is not null";
+//                try{
+//                    ResultSet rsczasQuery = stmt.executeQuery(czasQuery);
+//                    String przystanek = rsczasQuery.getString("przystanek");
+//                    String czas = rsczasQuery.getString("10"+i);
+//                    System.out.println(czas);
+//                }catch (Exception ee){
+//                    ee.printStackTrace();
+//                }
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
     public void getStopAndTime(short linia, short kurs){
+        Connect.linia = linia;
         Connect.kurs = kurs;
-        String getHighestId = "SELECT id FROM kurs101 order by id desc limit 1";
+        //kurs ++ while next table isn't null
         try{
-            Connection conn = this.connect(linia);
+
+            short kursNumber = 101;
+            Connection conn = this.connect();
             Statement stmt = conn.createStatement();
-            ResultSet rsId = stmt.executeQuery(getHighestId);
-            id = rsId.getShort("id");
-            for(int i = 1; i <= id; i++){
-                String queryNazwa_przystanku = "select nazwa_przystanku from kurs" + Connect.kurs + " where id=" + i;
-                ResultSet rsNazwaPrzystanku = stmt.executeQuery(queryNazwa_przystanku);
-                String fromNazwa_przystanku = rsNazwaPrzystanku.getString("nazwa_przystanku");
-                String queryCzas = "select czas from kurs" + Connect.kurs + " where id=" + i;
-                ResultSet rsCzas = stmt.executeQuery(queryCzas);
-                String planned = rsCzas.getString("czas");
-                Random rand = new Random();
-                int actualArrivalSeconds = rand.nextInt(11, 48);
-                String actualArrival = planned.substring(0, 6) + actualArrivalSeconds;
-                String arrivalDiff = "00:00:" + actualArrivalSeconds + "  ";
-                int actualDepartureSeconds = rand.nextInt(59);
-                while(actualDepartureSeconds < actualArrivalSeconds){
-                    if(actualArrivalSeconds >= 59){
-                        actualArrivalSeconds -= 11;
+            for (int j=1;j<=6;j++){
+                ResultSet rsHId = stmt.executeQuery("select id from linia813 where k"+kursNumber+" is not null order by id desc limit 1");
+                int highestId = rsHId.getInt("id");
+                ResultSet rsLowestId = stmt.executeQuery("select id from linia813 where k"+ kursNumber +" is not null");
+                int lowestId = rsLowestId.getInt("id");
+                System.out.println(lowestId);
+                System.out.println(highestId);
+                for(int i = lowestId; i <= highestId; i++){
+//                    System.out.println(i);
+                    ResultSet rsNazwaPrzystanku = stmt.executeQuery("select przystanek from linia813 where id=" + i);
+                    String fromNazwa_przystanku = rsNazwaPrzystanku.getString("przystanek");
+                    ResultSet rsCzas = stmt.executeQuery("select k"+kursNumber+" from linia813 where id=" + i);
+                    String planned = rsCzas.getString("k"+kursNumber);
+                    Random rand = new Random();
+                    int actualArrivalSeconds = rand.nextInt(11, 48);
+                    String actualArrival = planned.substring(0, 6) + actualArrivalSeconds;
+                    String arrivalDiff = "00:00:" + actualArrivalSeconds + "  ";
+                    int actualDepartureSeconds = rand.nextInt(59);
+                    while(actualDepartureSeconds < actualArrivalSeconds){
+                        if(actualArrivalSeconds >= 59){
+                            actualArrivalSeconds -= 11;
+                        }
+                        actualDepartureSeconds = rand.nextInt(actualArrivalSeconds + 10, 59);
                     }
-                    actualDepartureSeconds = rand.nextInt(actualArrivalSeconds + 10, 59);
+                    String actualDeparture = planned.substring(0, 6) + actualDepartureSeconds;
+                    String actualDepartureDiff = "00:00:" + actualDepartureSeconds;
+                    Przystanek defaultPrzystanek = new Przystanek(Utils.rightPadding("  " + fromNazwa_przystanku, ' ', 34), Utils.rightPadding(planned, ' ', 10), Utils.rightPadding(actualArrival, ' ', 10), Utils.rightPadding(arrivalDiff, ' ', 10), Utils.rightPadding(planned, ' ', 10), Utils.rightPadding(actualDeparture, ' ', 10), Utils.rightPadding(actualDepartureDiff, ' ', 10), "ok");
+                    output.add(defaultPrzystanek.nazwa + defaultPrzystanek.arrivalCzas + defaultPrzystanek.actualArrivalCzas + defaultPrzystanek.arrivalDiffCzas + defaultPrzystanek.departureCzas + defaultPrzystanek.actualDepartureCzas + defaultPrzystanek.departureDiffCzas + defaultPrzystanek.status);
                 }
-                String actualDeparture = planned.substring(0, 6) + actualDepartureSeconds;
-                String actualDepartureDiff = "00:00:" + actualDepartureSeconds;
-                Przystanek defaultPrzystanek = new Przystanek(Utils.rightPadding("  " + fromNazwa_przystanku, ' ', 34), Utils.rightPadding(planned, ' ', 10), Utils.rightPadding(actualArrival, ' ', 10), Utils.rightPadding(arrivalDiff, ' ', 10), Utils.rightPadding(planned, ' ', 10), Utils.rightPadding(actualDeparture, ' ', 10), Utils.rightPadding(actualDepartureDiff, ' ', 10), "ok");
-                output.add(defaultPrzystanek.nazwa + defaultPrzystanek.arrivalCzas + defaultPrzystanek.actualArrivalCzas + defaultPrzystanek.arrivalDiffCzas + defaultPrzystanek.departureCzas + defaultPrzystanek.actualDepartureCzas + defaultPrzystanek.departureDiffCzas + defaultPrzystanek.status);
-            }
-            for(Object a : output){
-                System.out.println(a);
-//                Files.write(Paths.get("./beginning1.txt"), a.toString().getBytes());
+                    for(Object a : output){
+                        System.out.println(a);
+                    }
+                    output.clear();
+                kursNumber++;
+
             }
         } catch(Exception e){
             System.out.println(e.getMessage());
--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -7,13 +7,13 @@ public class Writer{
     Connect app = new Connect();
 
     public void write(short linia, short kurs) throws IOException{
-        app.connect(linia);
-        app.getStopAndTime(linia, kurs);
+        app.connect();
+        app.getStopAndTime((short) 813, kurs);
 
 //        Files.copy(Path.of("src/main/java/org/example/beginning.txt"), Path.of("src/main/java/org/example/beginning1.txt"));
-        byte[] a = Files.readAllBytes(Path.of("./Schedules/beginning.txt"));
-        Files.write(Paths.get("./Schedules/" + Utils.nazwaRozkladu("06", "03", linia, kurs) + ".txt"), a);
-        Files.write(Paths.get("./Schedules/" + Utils.nazwaRozkladu("06", "03", linia, kurs) + ".txt"), app.output, StandardOpenOption.APPEND);
+//        byte[] a = Files.readAllBytes(Path.of("./Schedules/beginning.txt"));
+//        Files.write(Paths.get("./Schedules/" + Utils.nazwaRozkladu("06", "03", linia, kurs) + ".txt"), a);
+//        Files.write(Paths.get("./Schedules/" + Utils.nazwaRozkladu("06", "03", linia, kurs) + ".txt"), app.output, StandardOpenOption.APPEND);
     }
 }
 
